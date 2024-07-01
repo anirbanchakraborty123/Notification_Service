69f8adf593aa10f1fa0eb6d0927c6d451e8ed4e6
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2024-07-01 06:08:18 +0530

    updated file style

--- notifications_service/asgi.py
+++ notifications_service/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Notification_Service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notifications_service.settings")
 
 application = get_asgi_application()
--- notifications_service/wsgi.py
+++ notifications_service/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Notification_Service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notifications_service.settings")
 
 application = get_wsgi_application()
